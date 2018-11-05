c7115c80e70a3431685ceeb3fe1c0a4fd0c2714c
John Shitizen <34807761+Melted-Cheese96@users.noreply.github.com>
2018-11-05 20:03:48 +1000

    Add files via upload

--- pass_keep_third_build.py
+++ pass_keep_third_build.py
@@ -3,6 +3,8 @@ import os
 from time import sleep
 from passlib.hash import pbkdf2_sha256
 import sys
+import string 
+import random 
 
 def save_pass(user_password, file_name): #This saves a regular password
     with open(file_name, 'wb') as doc:
@@ -10,15 +12,23 @@ def save_pass(user_password, file_name): #This saves a regular password
 
 
 def save_master_pass(password): #This saves the master password
-    with open('cfg', 'wb') as doc:
+    with open('.cfg', 'wb') as doc:
         pickle.dump(password, doc)
     print('Your password has been saved!')
 
+def generate_password():
+    alphabet = string.ascii_lowercase
+    pWord = '' 
+    for letters in range(0,28):
+        random_letter = random.choice(alphabet)
+        pWord += random_letter
+        pWord += str(random.randint(1,10))
+    return pWord
 
 def verify_password(entered_password): #This function verifies the master password
     entered_password = str.encode(entered_password)
     print(type(entered_password))
-    doc = open('cfg', 'rb')
+    doc = open('.cfg', 'rb')
     content = pickle.load(doc)
     verification = pbkdf2_sha256.verify(entered_password, content)
     if verification == True:
@@ -37,11 +47,11 @@ def make_master_password(passw): #The master password is hashed with sha256
     passw = passw.encode('utf-8')
     hash1 = pbkdf2_sha256.hash(passw)
     print(type(hash1))
-    save_pass(hash1, 'cfg')
+    save_pass(hash1, '.cfg')
     print('Your password has been saved!')
-    print('Closing program...')
+    print('Restarting program...')
     sleep(1)
-
+    intro()
 
 def read_all_passwords(): #Shows the user all their saved passwords
     account = input('Enter what account you want to search for!')
@@ -62,19 +72,25 @@ def delete_all_files(): #This function removes everything if the master key file
     os.remove('.saved')
 
 
-
 def add_password(): #Lets the user add passwords
     new_password = {}
     all_files = os.listdir()
     account = input('Enter what this password is going to be used for!')
-    password = input('Enter password')
+    generate_pword = input('Do you want to generate a password?')
+    if generate_pword.lower() == 'yes' or generate_pword.lower() == 'y':
+        password = generate_password()
+        print('The password for this account is {}'.format(password))
+    elif generate_pword.lower() == 'no' or generate_pword.lower() == 'n':
+        password = input('Enter password')
     new_password[account] = password
     if account in all_files:
         print('You already have a password for that account!')
     else:
         with open('.saved', 'a') as doc:
-            doc.write(account)
+            doc.write('{}'.format(account))
+            doc.write('')
             print(doc)
+            
 
         save_pass(password, account)
     print('Your new password has been saved')
@@ -97,16 +113,27 @@ def delete_password():
                 pass
             else:
                 doc.write(line)
+                doc.write('\n')
         os.remove(account_to_delete)
+        print('Account details for {} has been deleted'.format(account_to_delete))
     else:
         print('That account was not found!')
         print('')
         sleep(1)
         main()
 
+def see_all_saved_accounts():
+    print('Here are all your saved passwords:')
+    with open('.saved') as doc:
+        for line in doc:
+            for word in line.split():
+                print('')
+                print(word)
+
 def main():
     while True:
-        option = input('Type in 1 to store a new password and type in 2 to retrieve all your passwords. Or type in 3 to delete one of your saved passwords. Or 4 to quit the program')
+        print('')
+        option = input('Type in 1 to store a new password and type in 2 to retrieve all your passwords, type in 3 to delete one of your saved passwords, type in 4 to quit the program or type in 5 to see all saved accounts')
         if option == '1':
             add_password()
         elif option == '2':
@@ -116,6 +143,12 @@ def main():
         elif option == '4':
             print('Quitting...')
             sys.exit()
+        elif option == '5':
+            see_all_saved_accounts()
+        else:
+            print('Invalid command...')
+            print('Restarting...')
+            main()
 
 def create_new_password(): #this is the function that prompts the user to enter a new master password if the file 'cfg' is not found
     print('It looks like this is your first time using this program!')
@@ -133,7 +166,7 @@ def intro():
     files = os.listdir()
     print('Welcome to PassKeep')
     print('')
-    if 'cfg' in files:
+    if '.cfg' in files:
         password = input('Enter your master password')
         verify_password(password)
         main()
@@ -145,3 +178,4 @@ def intro():
         sys.exit()
 
 intro()
+
