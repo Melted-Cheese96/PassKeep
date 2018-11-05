4f2e35f094dabcb803c33ad9827e7ff3900d09e2
John Shitizen <34807761+Melted-Cheese96@users.noreply.github.com>
2018-11-05 18:23:34 +1000

    Update pass_keep_third_build.py

--- pass_keep_third_build.py
+++ pass_keep_third_build.py
@@ -66,17 +66,17 @@ def delete_all_files(): #This function removes everything if the master key file
 def add_password(): #Lets the user add passwords
     new_password = {}
     all_files = os.listdir()
-    use_case = input('Enter what this password is going to be used for!')
+    account = input('Enter what this password is going to be used for!')
     password = input('Enter password')
-    new_password[use_case] = password
-    if use_case in all_files:
+    new_password[account] = password
+    if account in all_files:
         print('You already have a password for that account!')
     else:
         with open('.saved', 'a') as doc:
-            doc.write(use_case)
+            doc.write(account)
             print(doc)
 
-        save_pass(password, use_case)
+        save_pass(password, account)
     print('Your new password has been saved')
     print('')
     print('Returning...')
@@ -84,13 +84,38 @@ def add_password(): #Lets the user add passwords
     print('')
     main()
 
+def delete_password():
+    all_files = os.listdir()
+    account_to_delete = input('Enter account that you want to delete')
+    if account_to_delete in all_files:
+        doc = open('.saved', 'r')
+        content = doc.readlines()
+        doc.close()
+        doc = open('.saved', 'w')
+        for line in content:
+            if line == account_to_delete:
+                pass
+            else:
+                doc.write(line)
+        os.remove(account_to_delete)
+    else:
+        print('That account was not found!')
+        print('')
+        sleep(1)
+        main()
+
 def main():
     while True:
-        option = input('Type in 1 to store a new password and type in 2 to retrieve all your passwords')
+        option = input('Type in 1 to store a new password and type in 2 to retrieve all your passwords. Or type in 3 to delete one of your saved passwords. Or 4 to quit the program')
         if option == '1':
             add_password()
         elif option == '2':
             read_all_passwords()
+        elif option == '3':
+            delete_password()
+        elif option == '4':
+            print('Quitting...')
+            sys.exit()
 
 def create_new_password(): #this is the function that prompts the user to enter a new master password if the file 'cfg' is not found
     print('It looks like this is your first time using this program!')
@@ -106,7 +131,6 @@ def create_saved_account_list():
 
 def intro():
     files = os.listdir()
-
     print('Welcome to PassKeep')
     print('')
     if 'cfg' in files:
@@ -116,7 +140,6 @@ def intro():
     else:
         if '.saved' in files:
             delete_all_files()
-
         create_new_password()
         create_saved_account_list()
         sys.exit()
